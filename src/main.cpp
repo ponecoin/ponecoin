246f7656a8e932e909463c0c9414f93938ef6eac
x86cam <x86cam@x86cams-Mac-Pro.local>
2014-12-24 14:44:47 -0600

    Merkel Hash and Genesis Block changed.

--- src/main.cpp
+++ src/main.cpp
@@ -2045,10 +2045,10 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x5a2e19825b4162f68602039040f1e05d9f924ff00a3aff7327ca6abd6f3279bc"));
+        assert(block.hashMerkleRoot == uint256("0x92b13055b4c0bf2c3548db34e55ef78016267151f267b3850295abee2840c591"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (false && block.GetHash() != hashGenesisBlock)
+        if (true && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
