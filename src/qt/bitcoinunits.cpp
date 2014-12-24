5a84564f92edfc9a2b3835571e64ee4898cccb0e
x86cam <x86cam@x86cams-Mac-Pro.local>
2014-12-24 14:21:09 -0600

    Based PONE off of LTC.

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("PONE");
+    case mBTC: return QString("mPONE");
+    case uBTC: return QString::fromUtf8("μPONE");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("PoneCoin");
+    case mBTC: return QString("Milli-PoneCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-PoneCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
